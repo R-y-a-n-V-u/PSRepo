51adcee04dc68720de4f3edbbbb011650fe4f7bc
Acthach <arthur.thach@gmail.com>
2025-04-28 11:47:24 -0700

    main file started

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import json
+import time
+from datetime import datetime
+from PS_scraper import fetch_gen9ou_replays
+from PS_json_cleaner import clean_showdown_replay
+
+"""def main():
+will call the PS scraper then take all the links and clean them using clean clean_showdown_replay()
+then save them directly to database."""
