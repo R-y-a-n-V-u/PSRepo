6a488033b7c08d712e46a4eeea38e40ef74cd354
unknown <raymond.h.kang@gmail.com>
2025-05-02 22:09:41 -0700

    created database, table and successfuly inserted data given json file name in updated db.py

--- db.py
+++ db.py
@@ -13,7 +13,7 @@ def connectToDB(host, user, password, database, port):
         host=host,
         user=user,
         password=password,
-        #database=database,
+        database=database,
         port=port
     )
     print("Done connecting")
@@ -30,6 +30,58 @@ def connectToDB(host, user, password, database, port):
 print("Attempting to connect to cloud service")
 cursor, conn = connectToDB(HOST, USER, PASSWORD, DATABASE, PORT)
 print("Out of connection function")
+
+def createDatabase(cursor, database):
+    # Create a new database
+    queryString = "CREATE DATABASE IF NOT EXISTS " + database
+    cursor.execute(queryString)
+    print("Database created or already exists")
+
+createDatabase(cursor, DATABASE)
+
+def createTable(cursor):
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS data (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        game_id VARCHAR(255) UNIQUE,
+        json TEXT,
+        elo INT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    """)
+    print("Table created successfully or table already created")
+
+createTable(cursor)
+
+import json
+
+def insertJSON(cursor, conn, json_file):
+    f = open(json_file, 'r')
+    data = json.load(f)
+    game_id = data["id"]
+    json_text = json.dumps(data)
+    elo = int(data["pre_battle"][2][-4:]) + int(data["pre_battle"][3][-4:])
+    sql = "INSERT IGNORE INTO data (game_id, json, elo) VALUES (%s, %s, %s)"
+    val = (game_id, json_text, elo)
+    cursor.execute(sql, val)
+    conn.commit()
+    f.close()
+    print("Successfully inserted row")
+
+
+insertJSON(cursor, conn, "gen9ou-2172099392_clean.json")
+
+def printRows(cursor):
+    query = "SELECT * FROM data"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+printRows(cursor)
 
 cursor.close()
 conn.close()
