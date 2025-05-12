d8762341bcf967a7a832ee4080d6248e30f349e7
Acthach <arthur.thach@gmail.com>
2025-05-12 11:35:40 -0700

    missing pokemon battle info

--- PS_scraper.py
+++ PS_scraper.py
@@ -61,11 +61,3 @@ def fetch_gen9ou_replays(page=1, limit=50):
         print(f"Error fetching replays: {e}")
         return []
     
-if __name__ == "__main__":
-    while True:
-        replays = fetch_gen9ou_replays()
-        print(replays)
-        print('########')
-        # TODO: connect to DB and save replays
-        # Wait for 15 minutes
-        time.sleep(15 * 60)
